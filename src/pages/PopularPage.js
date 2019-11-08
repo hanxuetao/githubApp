cbe25bf7bac0198b763834b1eb0b73d7c5e0ebed
Xuetao Han <haxneutao0917@live.com>
2019-11-08 14:59:25 +0200

    top tab navigation

--- src/pages/PopularPage.js
+++ src/pages/PopularPage.js
@@ -1,21 +1,47 @@
 import React, {Component} from 'react';
 import {StyleSheet, View, Text} from 'react-native'
+import {createMaterialTopTabNavigator}  from 'react-navigation-tabs'
+import {createAppContainer} from 'react-navigation'
 
 export default class PopularPage extends React.Component{
     render() {
+        const TabNavigator = createAppContainer(createMaterialTopTabNavigator(
+            {
+                PopularTab1:{
+                    screen: PopularTab,
+                    navigationOptions:{
+                        title:'Tab1'
+                    },
+                },
+                PopularTab2:{
+                    screen: PopularTab,
+                    navigationOptions:{
+                        title:'Tab2'
+                    },
+                },
+            },
+        ));
         return (
             <View style={styles.container}>
-                <Text>Popular Page</Text>
+                <TabNavigator/>
             </View>
         );
     };
 };
 
+class PopularTab extends Component{
+    render(){
+        return (
+            <View style={styles.container}>
+                <Text>Popular Tab</Text>
+            </View>
+        )
+    }
+}
+
 const styles = StyleSheet.create({
     container:{
         flex: 1,
-        justifyContent:'center',
-        alignItems:'center',
         backgroundColor:'#F5FCFF',
     },
 });
